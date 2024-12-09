15598bce5437975e0c7016eedb30d47fa5b680a9
elsa <elsa@uab.com>
2024-12-09 10:15:57 +0100

    prova programes

--- practica4.c
+++ practica4.c
@@ -9,14 +9,25 @@ my_tee(const cadena){
 	f = open("cadena.txt", O_RDWR);
 	if (f == "-1"){
 		perror("Error en l'open");
-	}else{
-		while ((num = ssize_t read(cadena.txt,buf,3)) > 0){
-			int caracter = write(cadena.txt,buf,num);
-			if (caracter < 0){
-				perror("Error en el write");
-		}
-	printf(cadena.txt);
 	}
+	while ((num = ssize_t read(cadena.txt,buf,3)) > 0){
+		int caracter = write(cadena.txt,buf,num);
+		if (caracter < 0){
+			perror("Error en el write");
+		}
+	int close("cadena.txt")
+	}
+	printf(cadena.txt);
+
 }
 
 
+my_cmp(const arxiu1, const arxiu2){
+	int f,g;
+	f = open("arxiu1.txt", O_RDONLY);
+	g = open("arxiu2.txt", O_RDONLY);
+	if (f < 0 or g < 0){
+		perror("Error en l'open");
+	}
+	
+	while (
