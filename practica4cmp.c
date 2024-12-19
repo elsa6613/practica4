40875e91c789fb4280e8fb2afb6b4bcbab0bf37c
elsa <elsa@uab.com>
2024-12-19 13:12:51 +0100

    my_cmp acabat

--- practica4cmp.c
+++ practica4cmp.c
@@ -3,31 +3,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void main(const arxiu1, const arxiu2){
+int main(int argc, char *argv[]){
         int f,g;
-        f = open(arxiu1, O_RDONLY);
+	unsigned char buf1,buf2;
+	int byte = 1;
+	int linia = 1;
+	ssize_t n1, n2;
+
+        f = open(argv[1], O_RDONLY);
         if (f < 0){
                 perror("Error en l'open");
                 exit(-1);
         }
 
-        g = open(arxiu2, O_RDONLY);
+        g = open(argv[2], O_RDONLY);
         if (g < 0){
                 perror("Error en l'open");
                 exit(-1);
         }
 
-        int byte = 1;
-        int linia = 1;
-        while (ssize_t num1 = read(f,buf1,3)) > 0 && ssize_t num2 = read(g,buf2,3)) > 0){
-                byte++;
-                if (buf1 == '\n' && buf2 == '\n'){
-                        linia++;
+        while ((n1 = read(f,&buf1,1)) > 0 && (n2 = read(g,&buf2,1)) > 0){
+                if (buf1 != buf2){
+			printf("byte %d,line %d\n",byte,linia);
+			close(f);
+            		close(g);
                 }
-        }
-        if (buf1 != buf2){
-                printf(byte,linia);
-        }
-        int close(f)
-        int close(g)
+
+        	if (buf1 == '\n'){
+                	linia++;
+        	}
+        	byte++;
+	}
+	close(f);
+        close(g);
 }
